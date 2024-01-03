51148b1dfbe0e4d594688f56f13263acb922c46f
Gaz1l <abelgazil@gmail.com>
2024-01-03 08:09:04 +0000

    deploy v1.5 - idk

--- Backend/server.js
+++ Backend/server.js
@@ -42,7 +42,7 @@ const upload = multer({ dest: './files' });
 //console.log(process.env.NODE_ENV)
 //app.set('trust proxy', false);
 
-app.set('trust proxy', 2)
+app.set('trust proxy', 3)
 app.get('/ip', (request, response) => response.send(request.ip))
 app.get('/x-forwarded-for', (request, response) => response.send(request.headers['x-forwarded-for']))
 
